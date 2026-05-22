b503e0a1ec76e2b24c1d938e3851e6a00a3e6be2
mrHussain07 <hussainbscs2024@gmail.com>
2026-05-22 21:26:40 +0500

    Basics

---
+++ 01 Basics/script.js
@@ -0,0 +1,21 @@
+// ======================
+//       prompt()
+// ======================
+
+// prompt("Hye! How are you?");
+// let promptValue = prompt("Hye! How are you?");
+// console.log(promptValue);
+
+// ======================
+//       alert()
+// ======================
+
+// alert("Please! stay alert");
+
+// ======================
+//       confirm()
+// ======================
+
+// confirm("Are you fine?");
+let confirmValue = confirm("Are you fine?");
+console.log(confirmValue);
