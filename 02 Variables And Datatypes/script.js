a4cd466277e9c7091f50ae39d59d2cd013ad9926
mhussainilyas <mhussainilyas7@gmail.com>
2026-05-22 22:31:39 +0500

    variables and datatypes

---
+++ 02 Variables And Datatypes/script.js
@@ -0,0 +1,65 @@
+// ===========================
+//          console
+// ===========================
+
+// console.log("Muhammad Hussain");
+// console.info("This is info message");
+// console.warn("This is warning message");
+// console.error("This is error message");
+// console.table(["Hussain", "Suleman", "Zaryab"]);
+
+// ===========================
+//          Variables
+// ===========================
+
+// var a = 10;
+// let b = 20;
+// const c = 30;
+// console.log(a, b, c);
+
+// ===========================
+//      Types of Cases
+// ===========================
+
+// camelCase
+// PascalCase
+// snake_case
+// kabab-case
+
+// ===========================
+//    Primitive Datatypes
+// ===========================
+
+// 📝 NNSSBBU
+
+// let number_dt = 1039;
+// let null_dt = null;
+// let string_dt = "Hussain";
+// let symbol_dt = Symbol("This is symbol");
+// let boolean_dt = true;
+// let bigint_dt = 123456789123456789n;
+// let undefined_dt;
+
+// =============================
+//    Non Primitive Datatypes
+// =============================
+
+// const array_dt = ["Hussain", 21, true];
+// const object_dt = { name: "Hussain", age: 21, isStudent: true };
+// const set_dt = new Set([1, 2, 3, 4, 5]);
+// const map_dt = new Map([
+//   ["a", 1],
+//   ["b", 2],
+//   ["c", 3],
+// ]);
+
+// ===========================
+//          typeof()
+// ===========================
+
+// let a = "Hussain";
+// let b = 21;
+// let c = false;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
