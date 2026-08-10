e98cfcc8aac28d1a226f58b4f6848281fd9c3e15
mhussainilyas <mhussainilyas7@gmail.com>
2026-08-10 12:59:06 +0500

    intermediate question done

--- 15 Interview Questions/intermediate-questions.js
+++ 15 Interview Questions/intermediate-questions.js
@@ -85,3 +85,212 @@
 // let delElem = arr.splice(-4, 2, "x", "y");
 // console.log(delElem);
 // console.log(arr);
+
+// ======================
+//     Question # 21
+// ======================
+// 💡 Create a function that reverse each word of a given sentence. E.g., Mai hun hussain → niassuh nuh iaM
+
+// let str = "Mai hun Hussain";
+
+// function reverseSentence(str) {
+//   let res = str
+//     .split(" ")
+//     .map((word) => word.split("").reverse().join(""))
+//     .reverse()
+//     .join(" ");
+//   return res;
+// }
+
+// console.log(reverseSentence(str));
+
+// ======================
+//     Question # 22
+// ======================
+// 💡 In an array of numbers and strings, only add those numbers which are not strings.
+
+// let add = 0;
+// let mixArr = [10, "123", 27, 98, "100", "243"];
+
+// mixArr.forEach((item) => {
+//   if (typeof item === "number") {
+//     add += item;
+//   }
+// });
+
+// console.log(`sum of numbers is ${add}`);
+
+// ======================
+//     Question # 23
+// ======================
+// 💡 How would you check if a number is an integer?
+
+// let num = 100;
+// let num = 198.42;
+
+// 1st method
+// let res = Number.isInteger(num);
+
+// 2nd method
+// let isInteger = num % 1 === 0;
+
+// console.log(isInteger);
+
+// ======================
+//     Question # 24
+// ======================
+// 💡 Write a JavaScript function that reverse a number.
+
+// let num = 35426;
+
+// let reverseNum = Number(num.toString().split("").reverse().join(""));
+
+// console.log(reverseNum);
+
+// ======================
+//     Question # 25
+// ======================
+// 💡 Write a javascript function that returns a passed string with letters in alphabetical order.
+
+// let str = "i am muhammad hussain ilyas";
+
+// function alphabeticOrderStr(str) {
+//   return str.split(" ").sort().join(" ");
+// }
+
+// console.log(alphabeticOrderStr(str));
+
+// ======================
+//     Question # 26
+// ======================
+// 💡 Write a Javascript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case.
+
+// let str = "hye i am muhammad hussain ilyas";
+
+// function capitalizeStr(str) {
+//   return str
+//     .split(" ")
+//     .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+//     .join(" ");
+// }
+
+// console.log(capitalizeStr(str));
+
+// ======================
+//     Question # 27
+// ======================
+// 💡 Write a javascript function to get the first element of an array. Passing a parameter 'n' will return the first 'n' elements of the array
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+
+// function getFirstElement(arr, n) {
+//   if (!n) {
+//     return arr[0];
+//   } else if (n > arr.length) {
+//     console.error("value is out of bound!");
+//     return -1;
+//   } else {
+//     return arr.slice(0, n);
+//   }
+// }
+
+// console.log(getFirstElement(arr));
+// console.log(getFirstElement(arr, 5));
+// console.log(getFirstElement(arr, 10));
+
+// ======================
+//     Question # 28
+// ======================
+// 💡 Write a javascript function to get the number of occurrences of each letter in specified string.
+
+// let str = "Muhammad Hussain Ilyas";
+
+// function numberOfOccurrences(str) {
+//   let freqObj = {};
+//   let arr = str.replaceAll(" ", "").split("");
+
+//   arr.forEach((letter) => {
+//     if (freqObj.hasOwnProperty(letter)) {
+//       freqObj[letter] = freqObj[letter] + 1;
+//     } else {
+//       freqObj[letter] = 1;
+//     }
+//   });
+
+//   for (let key in freqObj) {
+//     console.log(`${key} occurs ${freqObj[key]} times`);
+//   }
+// }
+
+// numberOfOccurrences(str);
+
+// ======================
+//     Question # 29
+// ======================
+// 💡 write a javascript program to find the most frequent item of an array.
+
+// let arr = [1, 2, 3, 4, 2, 5, 3, 2, 5, 4, 7, 8, 1, 3];
+
+// function getMostFreqItem(arr) {
+//   let freqObj = {};
+
+//   arr.forEach((item) => {
+//     if (freqObj.hasOwnProperty(item)) {
+//       freqObj[item] = freqObj[item] + 1;
+//     } else {
+//       freqObj[item] = 1;
+//     }
+//   });
+
+//   let maxFreq = Math.max(...Object.values(freqObj));
+
+//   let freqObjArr = Object.entries(freqObj);
+//   let freqArr = freqObjArr.filter((arr) => arr[1] === maxFreq);
+
+//   console.log("the most frequent items of an array: ");
+//   freqArr.forEach((arr) => {
+//     console.log(arr[0]);
+//   });
+// }
+
+// getMostFreqItem(arr);
+
+// ======================
+//     Question # 30
+// ======================
+// 💡 write a javascript program to shuffle an array.
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// === 👉 shuffle original array ===
+// function shuffleArr(arr) {
+//   for (let index in arr) {
+//     let randomIndex = Math.floor(Math.random() * arr.length);
+
+//     // === 1st method of swaping ===
+//     // let temp = arr[index];
+//     // arr[index] = arr[randomIndex];
+//     // arr[randomIndex] = temp;
+
+//     // === 2nd method of swaping ===
+//     [arr[randomIndex], arr[index]] = [arr[index], arr[randomIndex]];
+//   }
+
+//   return arr;
+// }
+
+// === 👉 return new array by shuffling original array ===
+// function shuffleArr(arr) {
+//   let newArr = [];
+//   let tempArr = [...arr];
+
+//   while (tempArr.length > 0) {
+//     let randomIndex = Math.floor(Math.random() * tempArr.length);
+//     newArr.push(tempArr[randomIndex]);
+//     tempArr.splice(randomIndex, 1);
+//   }
+
+//   return newArr;
+// }
+
+// console.log(shuffleArr(arr));
